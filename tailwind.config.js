cb9406dd574b57cd5106b8f0c0053f5daea63dfb
0491384 <w0491384@nscc.ca>
2023-11-14 14:20:39 -0400

    complete main content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'apple-nav': '#323232',
+        'apple-nav-text': '#b4b5b4',
+        'apple-items': '#fafafa',
+      },
+    },
   },
   plugins: [],
 }
